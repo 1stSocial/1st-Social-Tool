ad8d293da4669fe70ded6a49ea460b49e2cacaca
Greg <gkodikara@gmail.com>
2013-03-15 12:27:16 +1100

    Changed order for header error

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -7,8 +7,8 @@ class Login extends CI_Controller {
 	}
 
 	function index() {
+		$this->load->view('header');
 		$this->load->helper(array('form'));
-     	$this->load->view('header');
 		$this->load->view('login');
      	$this->load->view('footer');
 	}
