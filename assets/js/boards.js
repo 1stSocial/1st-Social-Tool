0801f62188aee10846035c2e7b12472bdfee2cb5
Greg <gkodikara@gmail.com>
2013-03-18 09:51:09 +1100

    Mass commit

--- assets/js/boards.js
+++ assets/js/boards.js
@@ -1,22 +1,34 @@
-fnCreateDataTable("board_table");
+fnCreateDataTable();
 
-function fnCreateDataTable(sTableName) {
+//To go in main
+$("[rel='tooltip']").tooltip();
+
+function fnShowLoader() {
+	$(".loader").show();
+}
+
+function fnHideLoader() {
+	$(".loader").hide();
+}
+
+function fnRefreshTable() {
+	$.get("board_controller/get_board_table_html/true", function(response){
+		fnHideLoader();
+		$(".table-container").html(response);
+	}, "json");
+}
+
+function fnInitTable(sTableName) {
 	$("#" + sTableName).dataTable({
 		"sPaginationType": "full_numbers"
 	});
+}
+
+function fnCreateDataTable() {
+	fnInitTable("board_table");
 
 	var sAddBoardButton = "<button class='btn create-board'><i class='icon-plus'></i> Create New Board</button>";
 	$(".dataTables_filter").after(sAddBoardButton);
-
-	$(".entry-editable").hide();
-
-	$(".table-container").undelegate(".board-edit", "click");
-	$(".table-container").delegate(".board-edit", "click", function(){
-		$(this).toggle();
-		var oTableRow = $(this).parent("td").parent("tr");
-		oTableRow.find(".entry-editable, .entry-static").toggle();
-		$(oTableRow).addClass("editable");
-	});
 
 	$(".table-container").undelegate(".html-modal-open", "click");
 	$(".table-container").delegate(".html-modal-open", "click", function(){
@@ -27,7 +39,7 @@ function fnCreateDataTable(sTableName) {
 		var sBoardUrl = oTableRow.find(".board-url-holder").text();
 
 		$(".html-modal .modal-body #preview").html(sBoardHtml);
-		$(".html-modal .modal-body #edit .html-modal-name").val(sBoardName);
+		$(".html-modal .modal-body #edit .html-modal-name, .html-modal .modal-header h3 span").text(sBoardName);
 		$(".html-modal .modal-body #edit .html-modal-url").val(sBoardUrl);
 		$(".html-modal .modal-body #edit textarea").val(sBoardHtml);
 		
@@ -35,9 +47,42 @@ function fnCreateDataTable(sTableName) {
 		$("#jobs").html("<span class='text-center'>Jobs will not appear until you preview the feed</span>")
 	});
 
+	$(".table-container").undelegate(".board-preview", "click");
+	$(".table-container").delegate(".board-preview", "click", function(){
+		var oTableRow = $(this).parent("td").parent("tr");
+		var sBoardName = oTableRow.find(".board-name-holder").text();
+		window.open("/1st-Social-Tool/index.php/board_controller/show_board/" + sBoardName, '_newtab');
+	});
+
 	$(".html-modal").undelegate(".save-board-html", "click");
 	$(".html-modal").delegate(".save-board-html", "click", function(){
 		$("#preview").html($("#edit textarea").val());
+	});
+
+	$(".html-modal").undelegate(".save-board", "click");
+	$(".html-modal").delegate(".save-board", "click", function(){
+		var oData = {
+			"board_name": $(".html-modal .modal-body #edit .html-modal-name").text(),
+			"board_url": $(".html-modal .modal-body #edit .html-modal-url").val(),
+			"board_html": $(".html-modal .modal-body #edit textarea").val()
+		};
+
+		$.ajax({
+			url: "board_controller/modify_board",
+			data: oData,
+			type: "POST",
+			dataType: "json",
+			beforeSend: function(){
+				fnShowLoader();
+			},
+			success: function(response){
+				fnRefreshTable();
+				$(".html-modal").modal("hide");
+			},
+			error: function(error){
+				fnHideLoader();
+			}
+		});
 	});
 
 }
