6e45720f7c2921146c36c7bce9dcabf229dc7374
1st Social <gkodikara@1stexecutive.com.au>
2013-03-19 13:10:51 +1100

    need the script

--- application/views/rss_board.php
+++ application/views/rss_board.php
@@ -10,9 +10,9 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js" type="text/javascript"></script>
 <script src="<?= base_url(); ?>assets/js/lib/jquery.zrssfeed.min.js" type="text/javascript"></script>
-<!-- <script src="http://connect.facebook.net/en_US/all.js#appId=242689285769435&amp;xfbml=1"></script>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+
 <title>All Jobs</title>
- -->
 
 <script type="text/javascript">
 
