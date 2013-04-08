3cac6353c81e58495163e3ae81bab54574c32fa5
1st Social <gkodikara@1stexecutive.com.au>
2013-04-08 10:08:55 +1000

    Cpanel connect working

--- application/controllers/cpanel.php
+++ application/controllers/cpanel.php
@@ -4,12 +4,12 @@ class Cpanel extends CI_Controller {
 	
 	function __construct() {
 		parent::__construct();
-		$this->load->helper('file');
+		$this->load->helper("url");
+
 	}
 
 	function index() {
-
-		$this->create_subdomain("greg-sub");
+		//$this->create_subdomain("greg-sub");
 		$this->cpanel_authenticate();
 
 	}
@@ -20,35 +20,16 @@ class Cpanel extends CI_Controller {
 	}
 
 	private function cpanel_authenticate() {
-		$whmusername = "firstexe";
-		$whmpassword = "SDCjJDAWmLow";
 
-		$query = "https://gator332.hostgator.com:2083";
-
-		$curl = curl_init();		
-		# Create Curl Object
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);	
-		# Allow self-signed certs
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0); 	
-		# Allow certs that do not match the hostname
-		curl_setopt($curl, CURLOPT_HEADER,0);			
-		# Do not include header in output
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);	
-		# Return contents of transfer on curl_exec
-		$header[0] = "Authorization: Basic " . base64_encode($whmusername.":".$whmpassword) . "\n\r";
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $header);  
-		# set the username and password
-		curl_setopt($curl, CURLOPT_URL, $query);			
-		# execute the query
-		$result = curl_exec($curl);
-		if ($result == false) {
-		error_log("curl_exec threw error \"" . curl_error($curl) . "\" for $query");	
-		# log error if curl exec fails
-		}
-		curl_close($curl);
-
-		print $result;
+		$this->load->library('xmlapi', '184.172.173.27:2083');
+		// $this->$xmlapi = new xmlapi('184.172.173.27'); 
+		
+		$this->xmlapi->password_auth('firstexe','SDCjJDAWmLow'); 
+		// $this->xmlapi->set_debug(1); 
+		print $this->xmlapi->api1_query('accountname','SubDomain','addsubdomain',array('sub','domain.com',0,0,'/public_html/folder'));  
 	}
+
+
 
 
 
