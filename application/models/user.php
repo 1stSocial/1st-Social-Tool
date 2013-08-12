c11de32aa998d3a134267f6834e9faf6103e1e8c
unknown <apeksha.g@ignisitsolutions.com>
2013-08-12 12:14:59 +0530

    worked at setter and getter method for user model

--- application/models/user.php
+++ application/models/user.php
@@ -1,14 +1,140 @@
 <?php
 Class User extends CI_Model
 {
-    function login($username, $password) {
-        $this->db->select('id, username, password, user_level');
+	private $_id;
+	private $_name;
+	private $_domainId;
+	private $_username;
+	private $_password;
+	private $_type;
+	private $_accessLevel;
+	
+	/**
+	 * @return the $_id
+	 */
+	public function getId() {
+		return $this->_id;
+	}
+	
+	/**
+	 * @param field_type $_id
+	 */
+	public function setId($_id) {
+		$this->_id = $_id;
+	}
+	
+
+	/**
+	 * @return the $_name
+	 */
+	public function getName() {
+		return $this->_name;
+	}
+	
+	/**
+	 * @param field_type $_id
+	 */
+	public function setName($_name) {
+		$this->_name = $_name;
+	}
+	
+	/**
+	 * @return the $_domainId
+	 */
+	public function getDomainId() {
+		return $this->_domainId;
+	}
+	
+	/**
+	 * @param field_type $_domainId
+	 */
+	public function setDomainId($_domainId) {
+		$this->_domainId = $_domainId;
+	}
+	
+	/**
+	 * @return the $_username
+	 */
+	public function getUsername() {
+		return $this->_username;
+	}
+	
+	/**
+	 * @param field_type $_username
+	 */
+	public function setUsername($_username) {
+		$this->_username = $_username;
+	}
+	
+	/**
+	 * @return the $_password
+	 */
+	public function getPassword() {
+		return $this->_password;
+	}
+	
+	/**
+	 * @param field_type $_password
+	 */
+	public function setPassword($_password) {
+		$this->_password = $_password;
+	}
+	
+	/**
+	 * @return the $_type
+	 */
+	public function getType() {
+		return $this->_type;
+	}
+	
+	/**
+	 * @param field_type $_type
+	 */
+	public function setType($_type) {
+		$this->_type = $_type;
+	}
+	
+	/**
+	 * @return the $_accessLevel
+	 */
+	public function getAccessLevel() {
+		return $this->_accessLevel;
+	}
+	
+	/**
+	 * @param field_type $_accessLevel
+	 */
+	public function setAccessLevel($_accessLevel) {
+		$this->_accessLevel = $_accessLevel;
+	}
+	
+	
+	public function __construct(array $options = null)
+	{
+		parent::__construct();
+		if (null !== $options) {
+			foreach ($options as $option => $value) {
+				$setter = 'set'. ucfirst($option);
+				if (method_exists($this, $setter)) {
+					$this->{$setter}($value);
+				}
+			}
+			return $this;
+		}
+	}
+	
+	
+	
+	
+	
+    function login($username, $password) {    	
+        $this->db->select('id, username, password,access_level');
         $this->db->from('users');
         $this->db->where('username', $username);
         $this->db->where('password', MD5($password));
-        $this->db->limit(1);
-
-        $query = $this->db->get();
+        $this->db->limit(1);   
+      $query = $this->db->get();      
+    
         if ($query->num_rows() == 1) {
             return $query->result();
         }
