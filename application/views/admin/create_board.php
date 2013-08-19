8188fbc7d68d261f69368433ffab21248fd0dbbf
brijendra <brijendra@me.com>
2013-08-19 21:24:11 +0530

    application/views/admin/create_board.php create board with ajax and bootstrap popup.

--- application/views/admin/create_board.php
+++ application/views/admin/create_board.php
@@ -1,23 +1,29 @@
+<script>
+   setTimeout(function(){
+       $('#mod1').click();
+   },100);
+</script>
 
 
- <div class="container"> 
-     <h3 style="margin-left:5px;">Create Board</h3>
-    <p> <? if(isset($success)) echo $success;?> </p>
+<a href="#myModal1" role="button" id="mod1" style="display: none" class="btn" data-toggle="modal"></a>
+
 <?php  echo form_open('admin/home/create_board','class="horizontal-form"');  ?>
-<div class="component"><!-- Password input-->
-<div class="control-group">
-  <?php echo form_label('Board Name:', 'name', array('class' => "control-label") ); ?>
-  <div class="controls">
-    <input type="text"  placeholder="Board Name" name="name" >
-   </div>
-</div>
+<div id="myModal1" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-header">
+    <h3 style="margin-left:5px;">Create Board</h3>
+    <p> <? if(isset($success)) echo $success;?> </p>
 </div>
 
-<div class="component">
+<div class="modal-body"><!-- Password input-->
+
+  <?php echo form_label('Board Name:', 'name', array('class' => "control-label",'style'=>"float:left;margin-left:10px") ); ?>
+  <div class="controls">
+      <input type="text" style="margin-left: 55px"  class = "control-label" placeholder="Board Name" name="name" >
+  </div>
 <div class="control-group">
    <?php echo form_label('Board Parent Tag:', 'parent_tag', array('class' => "control-label") ); ?>
   <div class="controls">
-    <select id="parentTag" name="parentTag" >
+      <select id="parentTag" name="parentTag" onselect="call()">
   <option>Select</option>
   <?  if(!empty($parenTag)): foreach($parenTag as $parentTag): ?>
   <option value="<?=$parentTag->name?>"><?=$parentTag->name?></option>
@@ -25,21 +31,18 @@
 </select>
    </div>
 </div>
-</div>
+
      
-<div class="component">
 <div class="control-group">
    <?php echo form_label('Board Child Tag:', 'child_tag', array('class' => "control-label") ); ?>
   <div class="controls">
-    <select  id="childTagsId" name="tagId" >
+    <select  id="childTagsId" multiple name="tagId[]">
   <option>Select</option>
   
 </select>
    </div>
 </div>
-</div>     
 
-<div class="component"><!-- Partner-->
 <div class="control-group">
   <?php echo form_label('Board User (Partner):', 'user_id', array('class' => "control-label") ); ?>
   <div class="controls">
@@ -50,12 +53,29 @@
 </select>
    </div>
 </div>
-</div>
 
 
 <div class="control-group"> 
   <div class="controls">
-            <input type="submit" name="save" class="btn btn-primary" class="btn btn-primary" value="Create Board" />
+      <input type="button" style="float: right" class="close btn btn-primary" data-dismiss="modal" aria-hidden="true" value="Close">      
+      <input type="submit" style="float: right" name="save" class="btn btn-primary" value="Create Board" />
         </div>
   </div>
 </div>
+<script>
+    $('#parentTag').change(function()
+    {
+     $.ajax({
+         url:'../child_tag_list',
+         type:"post",
+         data:{'parent':this.value},
+         success:function(res)
+         {
+             $("#childTagsId").html(res);
+         }
+     });
+        
+    });
+</script>
+</div>
+ 
