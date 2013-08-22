153ab7a14fe6f21df225fc9dccea984f07ae5e75
brijendra <brijendra@me.com>
2013-08-22 17:35:57 +0530

    TagManagement.php tag management.

--- application/views/admin/TagManagement.php
+++ application/views/admin/TagManagement.php
@@ -1,7 +1,8 @@
 <table class="table table-striped ">
         <thead>
           <tr>
-            <th >Parent Tag</th>
+              <th>Tag Id</th> 
+            <th>Parent Tag</th>
             <th>Action</th>
           </tr>
         </thead>
@@ -9,9 +10,10 @@
             <?php if(is_array($parentTags) && !empty($parentTags)): 
                 foreach ($parentTags as $val): ?>
           <tr>
+            <td><?=$val->id?></td>  
             <td><?=$val->name?></td>
             <td><div class="btn-group"> <a onclick="edit(<?=$val->id?>)" class="btn btn-primary"><i class="icon-edit icon-white"></i> Edit</a><a onclick="return confirm('Are you sure?')" href="<?php echo site_url('/admin/home/delete_parenttag/'.$val->id) ?>"class="btn btn-danger"><i class="icon-trash icon-white"></i> Delete</a> </div></td>
-          </tr>
+   </tr>
           <? endforeach;endif;?>
         </tbody>
       </table>
@@ -27,7 +29,7 @@ function edit(val)
                             url:'tag',
 
                             type:'POST',
-                            data:{'parentTagid':val},
+                            data:{'id':val},
                             success:function(res)
                             {
                                 $('#edit').html(res);
@@ -35,25 +37,41 @@ function edit(val)
                             });
 }
 </script>
-
-
-
-<?php // echo site_url('/admin/home/edit_tag/'.$val->name) ?>
-<!--<a href="#myModal" role="button" id="mod" style="display: none" class="btn" data-toggle="modal"></a>
-
-
-<div id="myModal" class="modal hide fade">
-<div class="modal-header"><h3>Confirmation</h3></div>   
-<div class="modal-body" style="text-align: center">Are you sure?</div>
-<div class="modal-footer">
-    <a href="<?php //  echo site_url('/admin/home/delete_parenttag/'.$val->id) ?>" class="btn primary">OK</a>
-    <input type="button" id="close" class="close btn btn-primary" data-dismiss="modal" aria-hidden="true" value="Cancel">
-</div>
-</div>
-
 <script>
-    function cofirmation()
+   
+    function savefun()
     {
-        $('#mod').click();
+     var name = $('#name').val();
+     var id = $('#id').val();
+     
+     var dataval ={
+        parenttag : name,
+        Parentid : id      
+       };
+    $.ajax({
+        type: "POST",
+       url:"create_Tags",
+       data:dataval,
+       success:function(res){
+           if(res == '')
+               {
+                   setTimeout(function (){
+                    $('#close').click();
+                    window.location.href ='index';    
+                   },200);
+                   
+               }
+        else
+        $('#msg').text(res);
+        },
+       error:function(res)
+       {
+           alert(res);
+       }
+    });
     }
-</script>-->
+</script>
+
+
+
+           
