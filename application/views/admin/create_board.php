1d797938a13411308918ae3ca12eed65c4ee1a25
brijendra <brijendra@me.com>
2013-08-23 19:16:35 +0530

    create_board.php adding drop down.

--- application/views/admin/create_board.php
+++ application/views/admin/create_board.php
@@ -1,23 +1,6 @@
-<script src="<?= base_url(); ?>assets/js/chosen.jquery.js" type="text/javascript"></script>
-     <script src="<?= base_url(); ?>assets/css/docsupport/prism.js" type="text/javascript" charset="utf-8"></script> 
-     <script type="text/javascript">
-          var config = {
-            '.chosen-select'           : {},
-            '.chosen-select-deselect'  : {allow_single_deselect:true},
-            '.chosen-select-no-single' : {disable_search_threshold:10},
-            '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-            '.chosen-select-width'     : {width:"95%"}
-          }
-          for (var selector in config) {
-            jQuery(selector).chosen(config[selector]);
-          }
-          
-         // jQuery(document).ready(function(){  // it is better if u call your function inside document.ready function
-     jQuery(".chosen-select").chosen(); 
-      jQuery(".chosen-select- deselect").chosen({allow_single_deselect:true});
-  // });
-          
-</script> 
+<?php
+include '/dropdown.php';
+?>
 
 <script>
    setTimeout(function(){
