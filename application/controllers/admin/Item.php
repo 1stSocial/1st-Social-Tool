289fc96cb615a99c4af9cf7ebc16dcdcdd3ffff6
brijendra <brijendra@me.com>
2013-08-26 20:24:50 +0530

    Item.php adding bootstrap.

--- application/controllers/admin/Item.php
+++ application/controllers/admin/Item.php
@@ -13,10 +13,14 @@ class Item extends CI_Controller {
             $data['username'] = $session_data['username'];
             //Render Settings
             $data['settings'] = $session_data['access_level'] == 'super-admin' || $session_data['access_level'] == 'admin' ? "<a href='" . base_url() . "index.php/settings'><i class='icon-wrench icon-white'></i></a>" : false;
-
+            
+            $data['access_level'] =  $session_data['access_level'];
+            
             $this->load->view('admin/home', $data);
             //$this->load->view('footer');
-        } else {
+        } 
+        else 
+        {
             //If no session, redirect to login page
             redirect('/login');
         }
@@ -25,8 +29,9 @@ class Item extends CI_Controller {
     {
         $this->load->model('item_model');
          $this->load->helper('form');
-         
-         $data['items'] = $this->item_model->get_item();
+         $session_data = $this->session->userdata('logged_in');
+        
+         $data['items'] = $this->item_model->get_item($session_data['id']);
          $this->load->view('admin/manage_item',$data);
          
     }
@@ -34,17 +39,18 @@ class Item extends CI_Controller {
     function Add_item()
     {
         $this->load->helper('form');
-        $this->load->model('Board_model');
+        $this->load->model('item_model');
         $this->load->model('Tag_model');
         $this->load->model('Taxonomy_model');
         $session_data = $this->session->userdata('logged_in');
         
-        $boardModel = new Board_model();
-        $boards = $boardModel->getBoards($session_data['id']);
+        $itemModel = new Item_model();
+        $boards = $itemModel->get_board($session_data['id']);
 //         echo "<pre>";         var_dump($boards);
 //         die();
          $data['boards'] = $boards;
-        
+         $data['items'] = $this->item_model->get_item($session_data['id']);
+//         var_dump($data);
         $this->load->view('admin/add_item',$data);
         
     }
@@ -60,17 +66,20 @@ class Item extends CI_Controller {
         $boardModel = new Board_model();
         $taxonomy = new Taxonomy_model();
         $item = new Item_model();
-        
+        $session_data = $this->session->userdata('logged_in');
         $borad_id = $this->input->post('board');
         $boards = $boardModel->getBoardByBoardId($borad_id);
 //        var_dump($boards);
         $taxonomy = $item->get_taxonomy($boards['0']->parent_tags);
+        $tagid = $boardModel->bord_tag( $borad_id);// 
         $data['board_name'] = $boards['0']->name;
         $data['created_by'] = $boards['0']->created_by;
         $data['Taxonomy'] = $taxonomy;
+        $data['tag_id'] = $tagid['0']['tag_id'];// 
 //        var_dump($data['Taxonomy']);
 //       var_dump($data);
 //        die;
+        $data['items'] = $this->item_model->get_item($session_data['id']);
         $this->load->view('admin/item_fill',$data);
         
         
@@ -92,8 +101,11 @@ class Item extends CI_Controller {
         
         $itemid = $this->uri->segment(4);
         $item_modal = new Item_model();
+//        echo $itemid ; die;
         $data = $item_modal ->get_item_id($itemid);
-        $this->load->view('admin/item_edit',$data['0']);
+       // echo "id-";  print_r($data); die;
+       echo $this->load->view('admin/item_edit',$data['0'],TRUE);
+        die;
 //        var_dump($data);
     }
             
@@ -102,14 +114,17 @@ class Item extends CI_Controller {
         $this->load->model('Item_model');
         $item_model = new Item_model();
         $item_model->update_item();
-        $this->index();
+        echo '';
+        die;
     }
     
     function insert_item()
     { 
         $session_data = $this->session->userdata('logged_in');
-        $data = $this->input->post();
-//                        print_r($data); die;
+        $data['name'] = $this->input->post('name');
+        $data['title'] = $this->input->post('title');
+        $data['body'] = $this->input->post('body');
+        //                        print_r($data); die;
 //        $userId = $data['user_id'];
         
         unset($data['save']);
