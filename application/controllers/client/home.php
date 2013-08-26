6a2e5a4fe7cb38145f1883f8fabc312aa7cf3f86
brijendra <brijendra@me.com>
2013-08-26 20:27:18 +0530

    client

---
+++ application/controllers/client/home.php
@@ -0,0 +1,36 @@
+<?php
+ 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+session_start(); //we need to call PHP's session object to access it through CI
+
+class Home extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->view('header');
+        if ($this->session->userdata('logged_in')) {
+            $session_data = $this->session->userdata('logged_in');
+            $data['username'] = $session_data['username'];
+            //Render Settings
+            $data['settings'] = $session_data['access_level'] == 'super-client' || $session_data['access_level'] == 'client' ? "<a href='" . base_url() . "index.php/settings'><i class='icon-wrench icon-white'></i></a>" : false;
+            
+            $data['access_level'] =  $session_data['access_level'];
+            
+            $this->load->view('admin/home', $data);
+            //$this->load->view('footer');
+        } else {
+            //If no session, redirect to login page
+            redirect('/login');
+        }
+    }
+    
+    function index()
+    {
+        
+    }
+   
+
+}
+
+?>
