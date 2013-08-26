0e80731aa5c55a9d9d38d83ee43796e61edf24bc
brijendra <brijendra@me.com>
2013-08-26 11:59:14 +0530

    manage_item.php.

--- application/views/admin/manage_item.php
+++ application/views/admin/manage_item.php
@@ -1,14 +1,13 @@
 <table class="table table-striped ">
         <thead>
           <tr>
-              <th>Name</th> 
+            <th>Name</th> 
             <th>Title</th>
-           
             <th>Body</th>
-             <th>Created By</th>
-              <th>Status</th>
-                <th>Created Time</th>
-                  <th>Action</th>
+            <th>Created By</th>
+            <th>Status</th>
+            <th>Created Time</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -22,7 +21,7 @@
               <td><?=$val->created_by?></td>  
                <td><?=$val->status?></td>  
                <td><?=$val->createdTime?></td>  
-               <td><div class="btn-group"> <a onclick="edit(<?=$val->id?>)" class="btn btn-primary"><i class="icon-edit icon-white"></i> Edit</a><a onclick="return confirm('Are you sure?')" href="<?php echo site_url('/admin/item/delete_item/'.$val->id) ?>"class="btn btn-danger"><i class="icon-trash icon-white"></i> Delete</a> </div></td>
+               <td><div class="btn-group"> <a href="<?php echo site_url('/admin/item/edit_item/'.$val->id) ?>" class="btn btn-primary"><i class="icon-edit icon-white"></i> Edit</a><a onclick="return confirm('Are you sure?')" href="<?php echo site_url('/admin/item/delete_item/'.$val->id) ?>"class="btn btn-danger"><i class="icon-trash icon-white"></i> Delete</a> </div></td>
    </tr>
           <? endforeach;endif;?>
         </tbody>
