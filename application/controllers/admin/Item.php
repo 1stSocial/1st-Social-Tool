5adbbf4302ffff0f1b4bb0e1106a107b50798f1e
brijendra <brijendra@me.com>
2013-08-27 16:20:24 +0530

    Item.php add tag and taxonomy logic.

--- application/controllers/admin/Item.php
+++ application/controllers/admin/Item.php
@@ -75,10 +75,13 @@ class Item extends CI_Controller {
         $data['board_name'] = $boards['0']->name;
         $data['created_by'] = $boards['0']->created_by;
         $data['Taxonomy'] = $taxonomy;
+        
         $data['tag_id'] = $tagid['0']['tag_id'];// 
 //        var_dump($data['Taxonomy']);
 //       var_dump($data);
 //        die;
+        
+        
         $data['items'] = $this->item_model->get_item($session_data['id']);
         $this->load->view('admin/item_fill',$data);
         
@@ -124,19 +127,80 @@ class Item extends CI_Controller {
         $data['name'] = $this->input->post('name');
         $data['title'] = $this->input->post('title');
         $data['body'] = $this->input->post('body');
-        //                        print_r($data); die;
-//        $userId = $data['user_id'];
-        
-        unset($data['save']);
-        unset($data['user_id']);
-        $data['created_by'] = $session_data['id'];
-        $data['createdTime'] = date('Y-m-d h:m:s');
-        $data['status'] = '0';
         $this->load->model('item_model');
+        $error = array();
+        $error_iden = TRUE;
         $item = new Item_model();
         
-        $item->item_insert($data);
-        $this->index();
+        $tag_id = $this->input->post('tag_id');
+        
+        $taxoarr = $this->input->post('taxo');
+        foreach ($taxoarr as $taxo_id => $taxo_val) 
+            {
+                $val = $item->get_type($taxo_id);
+                 if($val)
+                 {
+                     switch($val['0']['type'])
+                     {
+                         case 'Integer':
+                         {
+                            if(!$taxo_val=='')
+                            {
+                             $exp = '/^[0-9]*[.]*[0-9]+$/' ;
+                             if(!preg_match($exp,$taxo_val))
+                             {
+                                 $error[] = $taxo_id.":Please Enter Number";
+                                 $error_iden = $error_iden && FALSE;
+                             }
+                             else
+                             {
+                                 $error[] = $taxo_id.":";
+                                 $error_iden = $error_iden && TRUE;
+                             }
+                            }
+                            else
+                            {
+                                $error[] = $taxo_id.":Please Enter Number";
+                                $error_iden = $error_iden && FALSE;
+                            }
+                         }
+                             break;
+                         case 'String':
+                         {
+                             if($taxo_val=='')
+                             {
+                                 $error[] = $taxo_id.":* please give value";
+                                 $error_iden = $error_iden && FALSE;
+                             }
+                             else
+                             {
+                                 $error[] = $taxo_id.":";
+                                 $error_iden = $error_iden && TRUE;
+                             }
+                         }
+                             break;
+                     }
+                 }
+            }
+        if($error_iden)
+        {
+//                $taxo = $this->input->post('taxo');
+//                $userId = $data['user_id'];
+
+                unset($data['save']);
+                unset($data['user_id']);
+                $data['created_by'] = $session_data['id'];
+                $data['createdTime'] = date('Y-m-d h:m:s');
+                $data['status'] = '0';
+                $item->item_insert($data);
+               echo '';
+                die();
+        }
+        else
+        {
+            echo json_encode($error);
+            die;
+        }
     }
 }
 ?>
