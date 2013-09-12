901a26a868f0dd8ddf5fcb7a8671c447b97862a3
brijendra <brijendra@me.com>
2013-09-12 20:36:28 +0530

    changes.

--- assets/js/custom/create_board.js
+++ assets/js/custom/create_board.js
@@ -1,11 +1,57 @@
 jQuery(document).ready(function() {
-     jQuery('.chosen-select').chosen(); 
+    jQuery('.chosen-select').chosen();
     setTimeout(function() {
         jQuery('#mod1').click();
     }, 100);
-    
-    jQuery('#theme_chosen').css('width','49%');
-    
+
+    jQuery('#theme_chosen').css('width', '49%');
+    jQuery('#taxo_chosen').css('width', '49%');
+
+    jQuery('#parentTag1').change(function()
+    {
+        var dataval = {
+            'tag_id': this.value
+        };
+        $('#taxo').empty();
+        
+        var site = $('#site').val() + '/admin/home/taxoval';
+        jQuery.ajax({
+            url: site,
+            data: dataval,
+            type: 'POST',
+            success: function(res)
+            {
+//                $('#taxo').append($('<option>', {
+//                    value: 1,
+//                    text: 'My option'
+//                }));
+                
+                var val = "<select data-placeholder='Choose a Filterable Taxonomy...' class=chosen-select style=width:350px; tabindex=4 id=taxo name=taxo>";
+                        val += "<option value=0></option>";
+                
+                 var obj = jQuery.parseJSON(res);
+                 $.each(obj,function (i,data){
+//                     alert(data['id']+":"+data['name']);
+                     val += "<option value="+data['id']+">"+data['name']+"</option>";
+                     
+                     
+                    });
+                 val +="</select>";
+//                 alert(val);
+                 $('#select_box').html("");
+                 $('#select_box').html(val);
+                     jQuery('.chosen-select').chosen();
+                     jQuery('#taxo_chosen').css('width', '49%');
+//                $('#chosen-select').typeahead({
+//                    updater:function (item) {
+//                     return item;
+//                    }
+//                });
+            }
+        });
+    });
+
+
 });
 
 jQuery('#closebtn').click(function() {
@@ -22,7 +68,7 @@ jQuery('#add').click(function() {
     var parentTag = jQuery('#parentTag1').val();
     var user_id = jQuery('#user_id').val();
     var theme_id = jQuery('#theme').val();
-
+    var taxo_id = jQuery('#taxo').val();
     if (name == "")
     {
         jQuery("#berror").css('padding-left:40%')
@@ -41,7 +87,7 @@ jQuery('#add').click(function() {
     {
         jQuery("#perror").hide();
     }
-   
+
 
     if (name != "" && parentTag != "0" && user_id != " ")
     {
@@ -49,7 +95,8 @@ jQuery('#add').click(function() {
             name: name,
             parentTag: parentTag,
             user_id: user_id,
-            theme_id:theme_id
+            theme_id: theme_id,
+            filterable_taxo :taxo_id
         };
         var ur = jQuery('#ur').val();
         jQuery.ajax({
