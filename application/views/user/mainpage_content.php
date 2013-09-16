4925dee82f62f0c86e8a156364557ec21e6da172
brijendra <brijendra@me.com>
2013-09-16 19:03:56 +0530

    work for image.

--- application/views/user/mainpage_content.php
+++ application/views/user/mainpage_content.php
@@ -1,168 +1,187 @@
 <div id="left">
-    <div id="latestjob"><li class="botwid widget_text">	<div class="textwidget"><? if(isset($board_name)) echo strtoupper($board_name); else echo 'LATEST JOBS';?></div>
+    <div id="latestjob"><li class="botwid widget_text">	<div class="textwidget"><? if (isset($board_name)) echo strtoupper($board_name);
+else echo 'LATEST JOBS'; ?></div>
         </li></div>
 
-   
+
     <div id="navigation" class="clearfix">
 
 
-                               <div style='margin-left: 80%'><?php echo $this->pagination->create_links();?></div>
-                                <!--</div>-->
-                                <input type="hidden" value="<?=$pageno;?>" id="pageno" name="pageno">
-                                <input type="hidden" value="<?=  site_url();?>" id="t" name="t">
+        <div style='margin-left: 80%'><?php echo $this->pagination->create_links(); ?></div>
+        <!--</div>-->
+        <input type="hidden" value="<?= $pageno; ?>" id="pageno" name="pageno">
+        <input type="hidden" value="<?= site_url(); ?>" id="t" name="t">
 
     </div>
-    <?php $loop = 0; if (is_array($post) && isset($post['item'])):foreach ($post['item'] as $val): ?>       
+<?php $loop = 0;
+if (is_array($post) && isset($post['item'])):foreach ($post['item'] as $val): ?>       
             <div class="tuts"> 
-                   <div id="tt">
-                <div id="lpouter" class="<?php if($loop%2) echo 'odd'; else echo'even'; ?> job_head_custom test"> <!--start lpouter-->
-                
-                    <div style="float:left;"> <!--start-->
-                        <div class="title">
+                <div id="tt">
+                    <div id="lpouter" class="<?php if ($loop % 2) echo 'odd';
+        else echo'even'; ?> job_head_custom test"> <!--start lpouter-->
 
-                            <h2><a rel="bookmark" title="Permanent Link to <?= $val->name; ?>"><?= $val->name; ?></a></h2>
-                            <div id="outer">
-                                <span class="location">Location: <?php if(isset($post['child'][$loop])):
-                                        foreach($post['child'][$loop] as $val1) :
-                                            if($val1->parent_tag_id == $post['Location'][0]->id):
-                                        ?>
-                                    <a href="javascript:footer_refine(<?=$val1->id?>)" rel="tag"><?=$val1->name?></a>
-                                    <?php endif; endforeach; endif;?></span><br>
-                                <span class="posted">Posted: <?php $dt = human_to_unix($val->createdTime);
+                        <div style="float:left;"> <!--start-->
+                            <div class="title">
+
+                                <h2><a rel="bookmark" title="Permanent Link to <?= $val->name; ?>"><?= $val->name; ?></a></h2>
+                                <div id="outer">
+                                    <span class="location">Location: <?php
+                                if (isset($post['child'][$loop])):
+                                    foreach ($post['child'][$loop] as $val1) :
+                                        if ($val1->parent_tag_id == $post['Location'][0]->id):
+                    ?>
+                                                    <a href="javascript:footer_refine(<?= $val1->id ?>)" rel="tag"><?= $val1->name ?></a>
+                <?php endif;
+            endforeach;
+        endif; ?></span><br>
+                                    <span class="posted">Posted: <?php
+        $dt = human_to_unix($val->createdTime);
         $formate = "%d %M %Y";
-        echo mdate($formate, $dt); ?></span> 
+        echo mdate($formate, $dt);
+        ?></span> 
+                                </div>
+
                             </div>
 
-                        </div>
+                        </div> <!--end-->   
+                        <div style="float:right; margin-right: 10px; margin-top: 22px;"> <!--start-->
+                            <img src="<?php if ($val->image != "") echo base_url() . '/' . $val->image;
+        else echo base_url() . '/assets/css/user/itemimage/default.png'; ?>" alt="admin" height="50" width="50">  
 
-                    </div> <!--end-->   
-                    <div style="float:right; margin-right: 10px; margin-top: 22px;"> <!--start-->
-                        <img src="<?php if($val->image!="") echo base_url().'/'.$val->image; else echo base_url().'/assets/css/user/itemimage/default.png'; ?>" alt="admin" height="50" width="50">  
+                        </div> <!--end-->
 
-                    </div> <!--end-->
+                    </div> <!--endl pouter-->
 
-                </div> <!--endl pouter-->
+                    <!--Start Jobs_Home-->
 
-                <!--Start Jobs_Home-->
+                    <div id="jobs_home1" style="display: none;" class="jobs_home_cont">
 
-                <div id="jobs_home1" style="display: none;" class="jobs_home_cont">
+                        <div class="tuts" id="post-226"> 
 
-                    <div class="tuts" id="post-226"> 
+                            <div class="title">
 
-                        <div class="title">
-
-                            <h1 style="padding-left:60px;"><a style="font-size:28px;" href="<?=  site_url()?>/user/user/detail/<?=$val->id?>" rel="bookmark" title="Permanent Link to <?= $val->title; ?>"><?= $val->title; ?></a></h1>
+                                <h1 style="padding-left:60px;"><a style="font-size:28px;" href="<?= site_url() ?>/user/user/detail/<?= $val->id ?>" rel="bookmark" title="Permanent Link to <?= $val->title; ?>"><?= $val->title; ?></a></h1>
 
 
-                            <div id="date"> <!--start date-->
-                                <span id="date1"><?php $dt = human_to_unix($val->createdTime);
+                                <div id="date"> <!--start date-->
+                                    <span id="date1"><?php
+        $dt = human_to_unix($val->createdTime);
         $formate = "%d";
-        echo mdate($formate, $dt); ?></span><br><span id="year"><?php $dt = human_to_unix($val->createdTime);
-        $formate = "%M, %Y";
-        echo mdate($formate, $dt); ?></span> 
-                            </div> <!--end date-->
+        echo mdate($formate, $dt);
+        ?></span><br><span id="year"><?php
+                                $dt = human_to_unix($val->createdTime);
+                                $formate = "%M, %Y";
+                                echo mdate($formate, $dt);
+                                ?></span> 
+                                </div> <!--end date-->
 
 
-                            <div class="postmeta"> 	
+                                <div class="postmeta"> 	
+                                    <span class="user">Posted by <a href="" title="Posts by admin" rel="author"><?php echo $val->user_name; ?></a></span>  
 
-                                <span class="user">Posted by <a href="" title="Posts by admin" rel="author"><?php echo $val->user_name; ?></a></span>  
+                                    <span class="clock"><?php
+                                        $dt = human_to_unix($val->createdTime);
+                                        $formate = "%l,%d %M %Y";
+                                        echo mdate($formate, $dt);
+                                        ?></span>  
 
-                                <span class="clock"><?php $dt = human_to_unix($val->createdTime);
-        $formate = "%l,%d %M %Y";
-        echo mdate($formate, $dt); ?></span>  
-
-                                <?php if(isset($post['salary'][$loop][0]->val)) :?>
-                                <span class="tags sallery_tag"><?=$post['salary'][$loop][0]->val;?>per year</span>
-                                <?php endif;?>
-                                <?php if(isset($post['parent'][$loop])) : foreach ($post['parent'][$loop] as $value) : ?>
-                                <span class="tags">
-                                    <?php if(isset($post['child'][$loop])):
-                                        foreach($post['child'][$loop] as $val1) :
-                                            if($val1->parent_tag_id == $value->parent_tag_id):
-                                        ?>
-                                    <a href="javascript:footer_refine(<?=$val1->id?>)" rel="tag"><?=$val1->name?></a>
-                                    <?php endif; endforeach; endif;?>
-                                </span>
-                                <?php endforeach; endif;?>
-                             
-
+        <?php if (isset($post['salary'][$loop][0]->val)) : ?>
+                                        <span class="tags sallery_tag"><?= $post['salary'][$loop][0]->val; ?>per year</span>
+                                    <?php endif; ?>
+        <?php if (isset($post['parent'][$loop])) : foreach ($post['parent'][$loop] as $value) : ?>
+                                            <span class="tags">
+                <?php
+                if (isset($post['child'][$loop])):
+                    foreach ($post['child'][$loop] as $val1) :
+                        if ($val1->parent_tag_id == $value->parent_tag_id):
+                            ?>
+                                                            <a href="javascript:footer_refine(<?= $val1->id ?>)" rel="tag"><?= $val1->name ?></a>
+                        <?php endif;
+                    endforeach;
+                endif; ?>
+                                            </span>
+            <?php endforeach;
+        endif; ?>
+                                </div>
                             </div>
+                            <div class="entry" style="padding:0 60px;">
+                                <div>
+        <?= $val->body ?>
+                                    <div></div>
+
+                                    <div></div>
+                                    <div></div>
+                                </div>
+
+                                <div class="clear"></div>
+                                <a class="readmore" href="">Apply Now</a>		
+                            </div>
+                            <div class="orange_saperator"> </div>
+                            <span id="list">
+                                <a id="viewlist" class="view_list">VIEW LIST</a> 
+                                <a href="http://1stworld.1stsocial.com.au/jobs/inside-sales-consultant-3/" id="nextjob" class="next_job">NEXT JOB</a>
+                                <div class="link_hidden" style="display:none;">
+                                    « <a href="http://1stworld.1stsocial.com.au/jobs/inside-sales-consultant-3/" rel="prev">Inside Sales Consultant</a>            </div>
+                            </span>
 
                         </div>
-                        <div class="entry" style="padding:0 60px;">
-                            <div>
-                                <?= $val->body ?>
-                                <div></div>
-                                
-                                <div></div>
-                                <div></div>
-                            </div>
+                    </div>
+                    <!--End Jobs_home-->
+                    <div class="entry">
 
-
-                            <a class="readmore" href="">Apply Now</a>			
-                          
-                            <div class="clear"></div>
-                        </div>
-                        <div class="orange_saperator"> </div>
-                        <span id="list">
-                            <a id="viewlist" class="view_list">VIEW LIST</a> 
-                            <a href="http://1stworld.1stsocial.com.au/jobs/inside-sales-consultant-3/" id="nextjob" class="next_job">NEXT JOB</a>
-                            <div class="link_hidden" style="display:none;">
-                                « <a href="http://1stworld.1stsocial.com.au/jobs/inside-sales-consultant-3/" rel="prev">Inside Sales Consultant</a>            </div>
-                        </span>
+                        <div class="clear"></div>
 
                     </div>
                 </div>
-                <!--End Jobs_home-->
-                <div class="entry">
-                   
-                    <div class="clear"></div>
-                    
-                </div>
-                </div>
             </div>
-    
-    <?php $loop++; endforeach;
-endif; ?>
-    <input type="hidden" name ="board_name" id="board_name" value="<?php if(isset($board_name)) { echo $board_name;}else{echo "home";} ?>">
+
+        <?php $loop++;
+    endforeach;
+endif;
+?>
+    <input type="hidden" name ="board_name" id="board_name" value="<?php if (isset($board_name)) {
+    echo $board_name;
+} else {
+    echo "home";
+} ?>">
 </div>
 
 <script>
 
-var shflag = 0 ;
-$("div#lpouter").click(function() {
+    var shflag = 0;
+    $("div#lpouter").click(function() {
 
-    $var = $(this).parent().find("#jobs_home1");
-               
-                $var.slideToggle('slow');
-            });
+        $var = $(this).parent().find("#jobs_home1");
+
+        $var.slideToggle('slow');
+    });
 
 
-$('#navigation a').click(function (){
-        
-         var id = $(this).text();
-         var pageno = $('#pageno').val();
+    $('#navigation a').click(function() {
 
-         $('#pageno').val(id);
-         
-});
+        var id = $(this).text();
+        var pageno = $('#pageno').val();
 
-$(document).ready()
-{
-      var str = $('#navigation strong').html();  
-     var value = $('#t').val();
-     var b_name = $('#board_name').val();
-     if(b_name == 'home')
+        $('#pageno').val(id);
+
+    });
+
+    $(document).ready()
+    {
+        var str = $('#navigation strong').html();
+        var value = $('#t').val();
+        var b_name = $('#board_name').val();
+        if (b_name == 'home')
         {
-        $('#navigation').html($('#navigation').html().replace('<strong>'+str+'</strong>','<a href='+value+'/user/user/index/'+str+'>'+str+'</a>'));
+            $('#navigation').html($('#navigation').html().replace('<strong>' + str + '</strong>', '<a href=' + value + '/user/user/index/' + str + '>' + str + '</a>'));
 
-     $('#navigation').html($('#navigation').html().replace('<a href='+value+'/user/user/index/'+$('#pageno').val()+'>'+$('#pageno').val()+'</a>','<strong>'+$('#pageno').val()+'</strong>'));
-     }  
-    else
-      {
-            $('#navigation').html($('#navigation').html().replace('<strong>'+str+'</strong>','<a href='+value+'/user/user/board/'+b_name+'/'+str+'>'+str+'</a>'));
+            $('#navigation').html($('#navigation').html().replace('<a href=' + value + '/user/user/index/' + $('#pageno').val() + '>' + $('#pageno').val() + '</a>', '<strong>' + $('#pageno').val() + '</strong>'));
+        }
+        else
+        {
+            $('#navigation').html($('#navigation').html().replace('<strong>' + str + '</strong>', '<a href=' + value + '/user/user/board/' + b_name + '/' + str + '>' + str + '</a>'));
 
-     $('#navigation').html($('#navigation').html().replace('<a href='+value+'/user/user/board/'+b_name+'/'+$('#pageno').val()+'>'+$('#pageno').val()+'</a>','<strong>'+$('#pageno').val()+'</strong>'));
-      }     
-}
+            $('#navigation').html($('#navigation').html().replace('<a href=' + value + '/user/user/board/' + b_name + '/' + $('#pageno').val() + '>' + $('#pageno').val() + '</a>', '<strong>' + $('#pageno').val() + '</strong>'));
+        }
+    }
 </script>
--- application/views/user/more_detail.php
+++ application/views/user/more_detail.php
@@ -1,59 +1,64 @@
 <div id="left">
- <?php $loop = 0; if (is_array($post) && isset($post['item'])):foreach ($post['item'] as $val): ?>       
-    
-       
-                    <div class="tuts" id="post-226"> 
-
-                        <div class="title">
-
-                            <h1 style="padding-left:60px;"><a style="font-size:28px;" href="" rel="bookmark" title="Permanent Link to <?= $val->title; ?>"><?= $val->title; ?></a></h1>
+    <?php $loop = 0;
+    if (is_array($post) && isset($post['item'])):foreach ($post['item'] as $val): ?>       
 
 
-                            <!--end date-->
+            <div class="tuts" id="post-226"> 
+
+                <div class="title">
+
+                    <h1 style="padding-left:60px;"><a style="font-size:28px;" href="" rel="bookmark" title="Permanent Link to <?= $val->title; ?>"><?= $val->title; ?></a></h1>
 
 
-                            <div class="postmeta"> 	
+                    <!--end date-->
 
-                                <span class="user">Posted by <a href="" title="Posts by admin" rel="author"><?php echo $val->user_name; ?></a></span>  
+                    <div class="postmeta"> 	
 
-                                <span class="clock"><?php $dt = human_to_unix($val->createdTime);
-        $formate = "%l,%d %M %Y";
-        echo mdate($formate, $dt); ?></span>  
+                        <span class="user">Posted by <a href="" title="Posts by admin" rel="author"><?php echo $val->user_name; ?></a></span>  
 
-                                <?php if(isset($post['salary'][$loop][0]->val)) :?>
-                                <span class="tags sallery_tag"><?=$post['salary'][$loop][0]->val;?> per year</span>
-                                <?php endif;?>
-                                <?php if(isset($post['parent'][$loop])) : foreach ($post['parent'][$loop] as $value) : ?>
+                        <span class="clock"><?php
+                            $dt = human_to_unix($val->createdTime);
+                            $formate = "%l,%d %M %Y";
+                            echo mdate($formate, $dt);
+                            ?></span>  
+
+                        <?php if (isset($post['salary'][$loop][0]->val)) : ?>
+                            <span class="tags sallery_tag"><?= $post['salary'][$loop][0]->val; ?> per year</span>
+                            <?php endif; ?>
+                            <?php if (isset($post['parent'][$loop])) : foreach ($post['parent'][$loop] as $value) : ?>
                                 <span class="tags">
-                                    <?php if(isset($post['child'][$loop])):
-                                        foreach($post['child'][$loop] as $val1) :
-                                            if($val1->parent_tag_id == $value->parent_tag_id):
-                                        ?>
-                                    <a href="javascript:footer_refine(<?=$val1->tag_id?>)" rel="tag"><?=$val1->name?></a>
-                                    <?php endif; endforeach; endif;?>
+                                    <?php
+                                    if (isset($post['child'][$loop])):
+                                        foreach ($post['child'][$loop] as $val1) :
+                                            if ($val1->parent_tag_id == $value->parent_tag_id):
+                                                ?>
+                                                <a href="javascript:footer_refine(<?= $val1->tag_id ?>)" rel="tag"><?= $val1->name ?></a>
+                        <?php endif;
+                    endforeach;
+                endif; ?>
                                 </span>
-                                <?php endforeach; endif;?>
-                             
-                            </div>
+                            <?php endforeach;
+                        endif; ?>
 
-                        </div>
-                        <div class="entry" style="padding:0 60px;">
-                            <div style="text-align: justify">
-                                <?= $val->body ?>
-                                <div></div>
-                                <div><a href="http://youtu.be/AGsYz2YTExg">&nbsp;</a></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-
-
-                            <a class="readmore" href="">Apply Now</a>			
-                           
-                            <div class="clear"></div>
-                        </div>
-                       
                     </div>
+
                 </div>
-    <?php $loop++; endforeach;
-endif; ?>
+                <div class="entry" style="padding:0 60px;">
+                    <div style="text-align: justify">
+        <?= $val->body ?>
+                        <div></div>
+                        <div><a href="http://youtu.be/AGsYz2YTExg">&nbsp;</a></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+
+                    <div class="clear"></div>
+                    <a class="readmore" href="">Apply Now</a>		
+                </div>
+            </div>
+        </div>
+        <?php $loop++;
+    endforeach;
+endif;
+?>
     
