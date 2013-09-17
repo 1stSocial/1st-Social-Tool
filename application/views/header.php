e65830eda57e48fabd2b2c7afdc57cc2764c4e53
brijendra <brijendra@me.com>
2013-09-17 15:07:51 +0530

    changes.

--- application/views/header.php
+++ application/views/header.php
@@ -1,90 +1,90 @@
 <?php date_default_timezone_set('Europe/London'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-     
+    <head>
+
         <link rel="stylesheet/less" href="<?= base_url(); ?>assets/css/user/temp.less"></link>
-         
+
         <script>
-        
-        var selectVar = null ; 
+
+            var selectVar = null;
         </script>
-         <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-         <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-         <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery-ui-1.10.1.custom.js"></script>
-         <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/redactor.js"></script>
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery-ui-1.10.1.custom.js"></script>
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/redactor.js"></script>
         <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.dataTables.min.js"></script>
-	
-	<script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.fileupload.js"></script>
-	<script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.iframe-transport.js"></script>
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.dataTables.min.js"></script>
+
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.fileupload.js"></script>
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.iframe-transport.js"></script>
         <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/bootbox.js"></script>
-        
-<script type="text/javascript" src="<?= base_url(); ?>assets/js/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+
+        <script type="text/javascript" src="<?= base_url(); ?>assets/js/bootstrap-fileupload/bootstrap-fileupload.js"></script>
         <script type="text/javascript" src="<?= base_url(); ?>assets/js/datatable/jquery.dataTables.js"></script>
         <script type="text/javascript" src="<?= base_url(); ?>assets/js/lib/jquery.form.js"></script>
         <script src="<?= base_url(); ?>assets/js/chosen.jquery.js" type="text/javascript"></script>
         <script src="<?= base_url(); ?>assets/css/docsupport/prism.js" type="text/javascript" charset="utf-8"></script> 
-         <script>
-        $(document).ready(function() {   
-         var config = {
-                    '.chosen-select'           : {},
-                    '.chosen-select-deselect'  : {allow_single_deselect:true},
-                    '.chosen-select-no-single' : {disable_search_threshold:10},
-                    '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-                    '.chosen-select-width'     : {width:'95%'}
-                  }
-                  for (var selector in config) {
-                    jQuery(selector).chosen(config[selector]);
-                  }
-
-                  jQuery(document).ready(function(){  // it is better if u call your function inside document.ready function
-             jQuery('.chosen-select').chosen(); 
-              jQuery('.chosen-select- deselect').chosen({allow_single_deselect:true});
-
-    
-    
-    });
-           
-           
-           
-           
-           
-        });
-                function myf()
-                {
-                    
-                    var sl = new SelectParser();
-      //   jQuery('.chosen-select').add_option();
+        <script>
+            $(document).ready(function() {
+                var config = {
+                    '.chosen-select': {},
+                    '.chosen-select-deselect': {allow_single_deselect: true},
+                    '.chosen-select-no-single': {disable_search_threshold: 10},
+                    '.chosen-select-no-results': {no_results_text: 'Oops, nothing found!'},
+                    '.chosen-select-width': {width: '95%'}
                 }
-         
-         </script>
-    
-       
-       
+                for (var selector in config) {
+                    jQuery(selector).chosen(config[selector]);
+                }
+
+                jQuery(document).ready(function() {  // it is better if u call your function inside document.ready function
+                    jQuery('.chosen-select').chosen();
+                    jQuery('.chosen-select- deselect').chosen({allow_single_deselect: true});
+
+
+
+                });
+
+
+
+
+
+            });
+            function myf()
+            {
+
+                var sl = new SelectParser();
+                //   jQuery('.chosen-select').add_option();
+            }
+
+        </script>
+
+
+
         <link rel="stylesheet" href="<?= base_url(); ?>assets/css/datatable/table.css">
-        <link rel="stylesheet" href="<?= base_url(); ?>assets/css/datatable/page.css">
-            <link rel="stylesheet" href="<?= base_url(); ?>assets/css/docsupport/style.css">
-        <link rel="stylesheet" href="<?= base_url(); ?>assets/css/docsupport/prism.css">
-        <link rel="stylesheet" href="<?= base_url(); ?>assets/css/chosen.css">
-        <style type="text/css" media="all">
-          /* fix rtl for demo */
-          .chosen-rtl .chosen-drop { left: -9000px; }
-        </style>
-        
-       
-        
-        
-             <!--<link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css"/>-->                                
- 	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="<?= base_url(); ?>assets/js/bootstrap-fileupload/bootstrap-fileupload.css">
- 	
- 	 <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"/>  
-        
- 	<link rel="stylesheet" href="<?= base_url(); ?>assets/less/main.css"/>
-        <link rel="stylesheet" href="<?= base_url(); ?>assets/css/redactor.css"/>
-   <title>1st Social</title>
- </head>
- <body>
-<div class="loader">
-</div>
+            <link rel="stylesheet" href="<?= base_url(); ?>assets/css/datatable/page.css">
+                <link rel="stylesheet" href="<?= base_url(); ?>assets/css/docsupport/style.css">
+                    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/docsupport/prism.css">
+                        <link rel="stylesheet" href="<?= base_url(); ?>assets/css/chosen.css">
+                            <style type="text/css" media="all">
+                                /* fix rtl for demo */
+                                .chosen-rtl .chosen-drop { left: -9000px; }
+                            </style>
+
+
+
+
+     <!--<link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css"/>-->                                
+                            <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css"/>
+                            <link rel="stylesheet" href="<?= base_url(); ?>assets/js/bootstrap-fileupload/bootstrap-fileupload.css">
+
+                                <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"/>  
+
+                                <link rel="stylesheet" href="<?= base_url(); ?>assets/less/main.css"/>
+                                <link rel="stylesheet" href="<?= base_url(); ?>assets/css/redactor.css"/>
+                                <title>1st Social</title>
+                                </head>
+                                <body>
+                                    <div class="loader">
+                                    </div>
