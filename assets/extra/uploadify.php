dd29f0b111c56ec39b6a96965af754d920af4df1
brijendra <brijendra@me.com>
2013-09-25 21:12:38 +0530

    changes.

--- assets/extra/uploadify.php
+++ assets/extra/uploadify.php
@@ -27,4 +27,26 @@ if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
 		echo 'Invalid file type.';
 	}
 }
+
+if(isset($_POST['id']))
+{
+    $tempFile = $_FILES['Filedata']['tmp_name'];
+    
+        if(!is_dir('../css/user/content/'.$_POST['id']))
+            mkdir('../css/user/content/'.$_POST['id']);
+        
+	$targetPath =  '../css/user/content/'.$_POST['id'];
+	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
+	
+	// Validate the file type
+	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
+	$fileParts = pathinfo($_FILES['Filedata']['name']);
+	
+	if (in_array($fileParts['extension'],$fileTypes)) {
+		move_uploaded_file($tempFile,$targetFile);
+		echo '1';
+	} else {
+		echo 'Invalid file type.';
+	}
+}
 ?>
