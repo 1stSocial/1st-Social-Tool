98d525558c69911efdbc09fdbcf06079dfae7cf9
brijendra <brijendra@me.com>
2013-10-17 20:57:05 +0530

    changes for adding call to action button and fb layout.

--- application/controllers/user/user.php
+++ application/controllers/user/user.php
@@ -7,13 +7,18 @@ class User extends CI_Controller {
         $this->load->model('user_model/user_model');
         $temp = new User_model;
         $theme = $temp->apply_theme();
-
-
+        $css = "";
+        
         $str = ""; 
 
         if (is_array($theme)) {
             foreach ($theme as $val) {
+                if($val['key'] != 'css_box')
                 $str .= '@' . $val['key'] . ':' . $val['value'] . '!important;';
+                else
+                {
+                    $css = $val['value'];
+                }
             }
         } else {
             $str = "";
@@ -27,7 +32,7 @@ class User extends CI_Controller {
         $isboardjobs1 = $this->uri->segment(1) ; 
         $isboardjobs2 = $this->uri->segment(2) ; 
         $isboardjobs3 = $this->uri->segment(3) ; 
-         
+       
         
         if($isboardjobs1 != "board" && $isboardjobs2!="board" )
         {
@@ -41,11 +46,35 @@ class User extends CI_Controller {
             flock($fp,LOCK_UN);
             fclose($fp);
             
-            
-            
-            if($this->uri->segment(5))
+//            echo $this->uri->segment(3);
+            if($this->uri->segment(3))
             {
-                $data_str =$this->uri->segment(5);
+//                echo $this->uri->segment(2);
+                
+                if($this->uri->segment(3) == 'fb')
+                {
+                     $_SESSION['fb'] = 'fb';
+                $data_str =$this->uri->segment(4);
+//                echo $data_str;
+                
+                }
+                else
+                {
+                    $_SESSION['fb'] = '';
+                    $data_str = $this->uri->segment(3);
+                }       
+                if($this->uri->segment(6) == 'fb' || $this->uri->segment(3) == 'detail')
+                {
+                    if($this->uri->segment(6) != 'fb')
+                    {
+                        $_SESSION['fb'] = '';
+                    }
+                    $data_str =$this->uri->segment(5);
+                    
+                }
+                
+                
+                
                 if($data_str)
                 {
                     $id = $temp ->get_boardid($data_str);
@@ -53,6 +82,7 @@ class User extends CI_Controller {
                     if($id!="")
                     {
                         $board_id = $id[0]->id;
+//                        echo $board_id;
                         $theme = $temp->apply_theme2($id[0]->id);
                     }else
                         $theme = $temp->apply_theme();    
@@ -65,7 +95,12 @@ class User extends CI_Controller {
                     $myarr = array();
                     if (is_array($theme)) {
                         foreach ($theme as $val) {
+                            if($val['key'] != 'css_box')
                             $str .= '@' . $val['key'] . ':' . $val['value'] . '!important;';
+                            else
+                            {
+                                $css = $val['value'];
+                            }
                         }  
                     } else {
                         $str = ""; 
@@ -80,40 +115,86 @@ class User extends CI_Controller {
                          flock($fp,LOCK_UN);
                          fclose($fp);
                          $data['board_name'] = $data_str;
-                         
+//                         echo $data_str;
                     }
                     else
-                {
-                    unset($data);
-                }
+                    {
+                        unset($data);
+                    }
                      if(isset($data));
                   
                 }
                 
                 
                 if(isset($data))
-                $this->load->view('user/header',$data);
-                else
                 {
-                if($isboardjobs1 == 'user' && $this->uri->segment(3) != 'board')
-                {
-                    $this->load->view('user/header');
+                    if($this->uri->segment(3) == 'fb' || $this->uri->segment(6) == 'fb')
+                    {
+                        
+                        $data['fb'] = 1;
+                         $_SESSION['fb'] = 'fb';
+                         $data['css'] = $css;
+                        $this->load->view('user/header',$data);
+//                        die;
+                    }
+                    else
+                    {
+                         $_SESSION['fb'] = '';
+                         if($this->uri->segment(3) == 'detail')
+                         {
+                            $data['css'] = $css;
+                            $this->load->view('user/header',$data);
+                         }
+                    }
                 }
+                else
+                { 
+                    if($this->uri->segment(3) == 'fb' || $this->uri->segment(6) == 'fb')
+                    {
+                        $data['fb'] = 1;
+                         $_SESSION['fb'] = 'fb';
+                         $data['css'] = $css;
+                        $this->load->view('user/header',$data);
+//                        die;
+                    }
+                    else
+                    {
+//                        $data['fb'] = 0;
+                        if($isboardjobs1 == 'user' && $this->uri->segment(3) != 'board' )
+                        {
+                             $_SESSION['fb'] = '';
+                             $data['css'] = $css;
+                            $this->load->view('user/header',$data);
+                        }
+                    }
+                    
+                    
+                
                 }
          }
        }
        $this->load->helper('date');
     }
 
-    public function fb()
+    public function fb($a = false,$data_str = FALSE)
     {
-//        $this->load->view('user/header',$data);
-    }
-    
-
-    public function board($data_str = FALSE)
-    {
-     
+//        $this->load->view('user/header_new',$data);
+        $_SESSION['fb'] = 'fb';
+        
+        if($this->uri->segment(3))
+            {
+//                echo $this->uri->segment(2);
+                
+                if($this->uri->segment(3) == 'fb')
+                {
+                $data_str =$this->uri->segment(4);
+//                echo $data_str;
+                
+                }
+                else
+                    $data_str = $this->uri->segment(3);
+                        
+            }
         $temp = new User_model;
      
         if($data_str)
@@ -137,7 +218,12 @@ class User extends CI_Controller {
         $myarr = array();
         if (is_array($theme)) {
             foreach ($theme as $val) {
+                if($val['key'] != 'css_box')
                 $str .= '@' . $val['key'] . ':' . $val['value'] . '!important;';
+                else
+                {
+                    $css = $val['value'];
+                }
             }  
         } else {
             $str = ""; 
@@ -152,7 +238,12 @@ class User extends CI_Controller {
              flock($fp,LOCK_UN);
              fclose($fp);
              $data['board_name'] = $data_str;
+              $values['fb'] = 1;
+              if($this->uri->segment(3) != 'fb')
+              {
+                  $values['css'] = $css;
              $this->load->view('user/header',$values);
+              }
         }
         
         //body start here..
@@ -160,7 +251,7 @@ class User extends CI_Controller {
         $this->load->model('tag_model');
         $tag_model = new Tag_model();
         $this->load->library('pagination');
-        $config['base_url'] = site_url() . '/user/user/board/'.$data_str ;
+        $config['base_url'] = site_url() . '/user/user/fb/'.$data_str ;
         $config['per_page'] = 5;
         $config['use_page_numbers'] = TRUE;
         $config['uri_segment'] = 5;
@@ -193,7 +284,7 @@ class User extends CI_Controller {
                 {
                 $data['post'] = $this->user_model->post_job($val * 5,$board_id);
                  $config['total_rows'] = $this->user_model->page_count('post_job',$board_id);
-                 
+                 $data['btn_name'] = $this->user_model->btn_name($board_id);
                 }
                 else {
                     $data['post'] = $this->user_model->post_job($val * 5);
@@ -240,7 +331,186 @@ class User extends CI_Controller {
             $this->pagination->initialize($config);
 //            echo $val ; die;
             if(isset($board_id))
+            {
+                 $data['btn_name'] = $this->user_model->btn_name($board_id);
                 $data['post'] = $this->user_model->post_job($val * 5,$board_id);
+            }
+                else {
+                    $data['post'] = $this->user_model->post_job($val * 5);
+                }
+                
+              
+                
+            $data['content'] = $this->load->view('user/mainpage_content', $data, TRUE);
+//         
+        }
+//       echo $board_id;
+        if(isset($board_id))
+        {
+            $data['tag'] = $tag_model->semi_parent($board_id);
+             $data['latestjob'] = $this->user_model->latest_job($board_id);
+        $data['max_min'] =$this->user_model->taxo_val($board_id);
+        }
+        else
+        {
+        $data['tag'] = $tag_model->semi_parent();
+         $data['latestjob'] = $this->user_model->latest_job();
+        $data['max_min'] =$this->user_model->taxo_val();
+        
+        }
+        
+       
+           
+//        $data['board_name'] = $
+          
+        $this->load->view('user/sidebar', $data);
+//        $this->load->view('user/footer', $data['tag']);
+
+    }
+    
+
+    public function board($data_str = FALSE)
+    {
+     
+        $temp = new User_model;
+        $css = "";
+        $_SESSION['fb'] = '';
+        
+        if($data_str)
+        {
+            $id = $temp ->get_boardid($data_str);
+            $values['main'] =$id;
+            if($id!="")
+            {
+                $board_id = $id[0]->id;
+                $data['title'] = $id[0]->board_title;
+                $theme = $temp->apply_theme2($id[0]->id);
+            }else
+                $theme = $temp->apply_theme();    
+        }
+        else
+        {
+            $theme = $temp->apply_theme();
+        }
+        
+        $str = "";
+        $myarr = array();
+        if (is_array($theme)) {
+            foreach ($theme as $val) {
+                if($val['key'] != 'css_box')
+                $str .= '@' . $val['key'] . ':' . $val['value'] . '!important;';
+                else
+                {
+                    $css = $val['value'];
+                }
+            }  
+        } else {
+            $str = ""; 
+        }
+       $filepath = 'assets/css/user/temp.less';
+      
+        if($temp->board_exist($data_str))
+        {    
+            $fp = fopen($filepath, "w+");
+             flock($fp,LOCK_EX);
+             fwrite($fp, $str);
+             flock($fp,LOCK_UN);
+             fclose($fp);
+             $data['board_name'] = $data_str;
+             $values['css'] =$css;
+             $this->load->view('user/header',$values);
+        }
+        
+        //body start here..
+        
+        $this->load->model('tag_model');
+        $tag_model = new Tag_model();
+        $this->load->library('pagination');
+        $config['base_url'] = site_url() . '/user/user/board/'.$data_str ;
+        $config['per_page'] = 5;
+        $config['use_page_numbers'] = TRUE;
+        $config['uri_segment'] = 5;
+        
+        $val = $this->uri->segment(5);
+//        echo $val;
+//        var_dump($val);die;
+        
+        $data['pageno'] = $val;
+      
+
+        if ($val > 1) {
+            $val = $val - 1;
+        } else {
+            $val = 0;
+        }
+
+        if ($this->input->post()) {
+            
+//            echo 'else';
+//            echo $val;
+//            echo $board_id;
+            
+            $id = $this->input->post('val');
+            $id_array = explode(",", $id);
+            if (count($id_array) - 1 == 0) {
+               
+               
+                if(isset($board_id))
+                {
+                $data['post'] = $this->user_model->post_job($val * 5,$board_id);
+                 $config['total_rows'] = $this->user_model->page_count('post_job',$board_id);
+                  $data['btn_name'] = $this->user_model->btn_name($board_id);
+                }
+                else {
+                    $data['post'] = $this->user_model->post_job($val * 5);
+                    $config['total_rows'] = $this->user_model->page_count('post_job');
+                }
+                $data['pageno'] = $this->input->post('pageno');
+//            $data['parent_id'] = $this->user_model->parent_id();
+                $choice = $config["total_rows"] / $config["per_page"];
+                $config["num_links"] = round($choice);
+                $this->pagination->initialize($config);
+                echo $this->load->view('user/mainpage_content', $data, TRUE);
+            } else {
+                if(isset($board_id))
+                $config['total_rows'] = $this->user_model->refine_post_count($id_array,$board_id);
+                else
+                $config['total_rows'] = $this->user_model->refine_post_count($id_array);
+                $config['base_url'] ="" ;
+                $choice = $config["total_rows"] / $config["per_page"];
+                $config["num_links"] = round($choice);
+                $this->pagination->initialize($config);
+                if(isset($board_id))
+                $data['post'] = $this->user_model->refine_post_job($id_array, $val * 5,$board_id);
+                else {
+                    $data['post'] = $this->user_model->post_job($val * 5);
+                }
+                
+                $data['pagename'] = 'sidebar_refine';
+                
+                $data['total_row'] = $config['total_rows'];
+                
+                echo $this->load->view('user/content', $data, TRUE);
+            }
+            die;
+        } else {
+            
+           
+             if(isset($board_id))
+            $config['total_rows'] = $this->user_model->page_count('post_job',$board_id);
+             else
+            $config['total_rows'] = $this->user_model->page_count('post_job');
+             
+             $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
+            $this->pagination->initialize($config);
+//            echo $val ; die;
+            if(isset($board_id))
+            {
+                $data['post'] = $this->user_model->post_job($val * 5,$board_id);
+            
+                 $data['btn_name'] = $this->user_model->btn_name($board_id);
+            }
                 else {
                     $data['post'] = $this->user_model->post_job($val * 5);
                 }
@@ -320,6 +590,8 @@ class User extends CI_Controller {
 
     public function index() {
        
+       
+        
         $this->load->model('tag_model');
         $tag_model = new Tag_model();
         $this->load->library('pagination');
@@ -389,12 +661,24 @@ class User extends CI_Controller {
 
         $this->load->library('pagination');
         
-        $config['base_url'] = site_url() . '/user/user/index';
+        $config['base_url'] = site_url() . '/user/user/footer_refine';
         $config['per_page'] = 5;
         $config['total_rows'] = $this->user_model->refine_post_count($id_array,$id_t);
+        $config['use_page_numbers'] = TRUE;
+        $config['uri_segment'] = 4;
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
         $this->pagination->initialize($config);
         $val = $this->uri->segment(4);
        
+        if($val == 0)
+        {
+            $val = 0;
+        }
+        else
+        {
+            $val = ($val - 1)*5;
+        }
         if(isset($board_id['0']->id))
          {
 //              var_dump($board_id);
@@ -408,6 +692,7 @@ class User extends CI_Controller {
          
         $data['pagename'] = 'footer_refine';
         $data['board_name']=$data_str;
+        $data['tag_id'] = $id;
          echo $this->load->view('user/content', $data, TRUE);
         die;
     }
@@ -423,6 +708,12 @@ class User extends CI_Controller {
         $config['total_rows'] = $this->user_model->page_count('refine_job_salary');
         else
         $config['total_rows'] = $this->user_model->page_count('refine_job_salary',$board_id['0']->id);
+        
+        $config['use_page_numbers'] = TRUE;
+        $config['uri_segment'] = 4;
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
+        
         
         $this->pagination->initialize($config);
         $val = $this->uri->segment(4);
@@ -486,6 +777,7 @@ class User extends CI_Controller {
       
         
         $this->load->view('user/sidebar', $data);
+        if($_SESSION['fb'] != 'fb')
         $this->load->view('user/footer', $data['tag']);
     }
 
@@ -495,6 +787,11 @@ class User extends CI_Controller {
         $board_id = $this->user_model->get_boardid($data_str);
         $this->load->library('pagination');
         $config['per_page'] = 5;
+        $config['use_page_numbers'] = TRUE;
+        $config['uri_segment'] = 4;
+        
+        
+        
         if($board_id!="")
          {
         $config['total_rows'] = $this->user_model->page_count('keyword_search',$board_id['0']->id);
@@ -512,6 +809,8 @@ class User extends CI_Controller {
         } else {
             $val = $this->uri->segment(4);
         }
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
         $this->pagination->initialize($config);
         if($board_id=="")
          {
