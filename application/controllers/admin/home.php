16c56efe4e131e7eb727e1f3ae8fff6607f68f47
brijendra <brijendra@me.com>
2013-10-22 16:37:37 +0530

    changes.

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -6,7 +6,7 @@ session_start(); //we need to call PHP's session object to access it through CI
 
 class Home extends CI_Controller {
 
-    function __construct() {
+    function __construct() { 
         parent::__construct();
         $this->load->view('header');
         if($this->session->userdata('logged_in')) {
