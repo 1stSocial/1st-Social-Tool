72e114400d7918777d07ae353b59fcd7fc35e075
brijendra <brijendra@me.com>
2013-10-22 16:39:37 +0530

    changes for edit board.

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -5,7 +5,7 @@ if (!defined('BASEPATH'))
 session_start(); //we need to call PHP's session object to access it through CI
 
 class Home extends CI_Controller {
-
+ 
     function __construct() { 
         parent::__construct();
         $this->load->view('header');
